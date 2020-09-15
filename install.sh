f263a4d642ba1174169b322f8ab774bce629aec0
sudoreboot2020 <55040629+kotik06@users.noreply.github.com>
2020-09-15 09:07:23 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 apt install python
 python3 -m pip install pyTelegramBotAPI==0.3.0
+python3 -m pip install pyautogui
 python3 -m pip install opencv-python
 python3 -m pip install colorama
 python3 -m pip install random
